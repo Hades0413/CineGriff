59c404f56d104465f99e9d6359d71003322fee30
mals24 <marceloliendo24@gmail.com>
2024-12-06 19:35:02 -0500

    V1.3

--- CineGriff/api/ListarGeneroViewController.swift
+++ CineGriff/api/ListarGeneroViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ListarGeneroViewController: UIViewController,UITableViewDataSource,UITableViewDelegate  {
+class ListarGeneroViewController: UIViewController,UITableViewDataSource,UITableViewDelegate, UISearchBarDelegate  {
     
     
     
@@ -15,7 +15,15 @@ class ListarGeneroViewController: UIViewController,UITableViewDataSource,UITable
     
     @IBOutlet weak var sbBuscarGenero: UISearchBar!
     
-    var lista:[Genero]=[]
+    var lista: [Genero] = [
+            Genero(codigo_Genero: 1, nombre_Genero: "Acción"),
+            Genero(codigo_Genero: 2, nombre_Genero: "Comedia"),
+            Genero(codigo_Genero: 3, nombre_Genero: "Drama"),
+            Genero(codigo_Genero: 4, nombre_Genero: "Terror"),
+            Genero(codigo_Genero: 5, nombre_Genero: "Ciencia Ficción")
+        ]
+        var listaFiltrada: [Genero] = []
+        var buscando: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,23 +31,42 @@ class ListarGeneroViewController: UIViewController,UITableViewDataSource,UITable
         tvGenero.dataSource = self
         tvGenero.delegate = self
         tvGenero.rowHeight = 130
+        
+        sbBuscarGenero.delegate = self
     }
     
     func listarGenero(){}
     
     func buscarGenero(nombre_Genero: String)->[Genero]{
-        
+        return lista.filter { $0.nombre_Genero.lowercased().contains(nombre_Genero.lowercased()) }
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            if searchText.isEmpty {
+                buscando = false
+                listaFiltrada = []
+            } else {
+                buscando = true
+                listaFiltrada = buscarGenero(nombre_Genero: searchText)
+            }
+            tvGenero.reloadData()
+        }
     
     @IBAction func btnNuevoGenero(_ sender: UIButton) {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return buscando ? listaFiltrada.count : lista.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let fila = tvGenero.dequeueReusableCell(withIdentifier: "row") as! ItemGeneroTableViewCell
+        
+        let genero = buscando ? listaFiltrada[indexPath.row] : lista[indexPath.row]
+        
+        fila.lblCodigoGenero.text = String(genero.codigo_Genero)
+        fila.lblNombreGenero.text = genero.nombre_Genero
+        return fila
     }
 
 }
