7c4843c43959fbb89c05a337cfcaa1211725bf41
mals24 <marceloliendo24@gmail.com>
2024-12-15 23:39:02 -0500

    V2.0

--- CineGriff/api/ListarPeliculaViewController.swift
+++ CineGriff/api/ListarPeliculaViewController.swift
@@ -8,35 +8,104 @@
 import UIKit
 import Alamofire
 
-class ListarPeliculaViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UISearchBarDelegate {
+class ListarPeliculaViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UISearchBarDelegate,UICollectionViewDelegate {
     
     @IBOutlet weak var cvPelicula: UICollectionView!
     
     @IBOutlet weak var sbBuscarTitulo: UISearchBar!
     
     var lista:[Pelicula]=[]
+    var listaFiltrada: [Pelicula] = []
+    var buscando: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listado()
+        cvPelicula.delegate = self
+        cvPelicula.dataSource = self
+        sbBuscarTitulo.delegate = self
+    }
     
+    func listado(){
+        AF.request("https://cinegriffapi-production.up.railway.app/api/pelicula/listar").responseDecodable(of: [Pelicula].self) { response in
+            switch response.result {
+            case .success(let peliculas):
+                self.lista = peliculas
+                self.cvPelicula.reloadData()
+            case .failure(let error):
+                print("Error al obtener las películas: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func buscarTitulo(tituloPelicula: String) {
+        
+        let searchText = tituloPelicula.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if searchText.isEmpty {
+            listaFiltrada = lista
+        } else {
+            listaFiltrada = lista.filter { pelicula in
+                return pelicula.tituloPelicula.lowercased().contains(searchText)
+            }
+        }
+        
+        self.buscando = true
+        self.cvPelicula.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let textoBusqueda = searchBar.text {
+            buscarTitulo(tituloPelicula: textoBusqueda)
+        }
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            listaFiltrada = lista
+            buscando = false
+        } else {
+            buscarTitulo(tituloPelicula: searchText)
+        }
+        cvPelicula.reloadData()
     }
     
     @IBAction func btnPelicula(_ sender: UIButton) {
+        performSegue(withIdentifier: "registrarPelicula", sender: nil)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return lista.count
+        return buscando ? listaFiltrada.count : lista.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let fila=collectionView.dequeueReusableCell(withReuseIdentifier: "row", for: indexPath) as! ItemPeliculaCollectionViewCell
-        let pelicula = lista[indexPath.row]
-        
+        let fila = collectionView.dequeueReusableCell(withReuseIdentifier: "row", for: indexPath) as! ItemPeliculaCollectionViewCell
+
+        let pelicula = buscando ? listaFiltrada[indexPath.row] : lista[indexPath.row]
+
+        fila.imgPelicula.image = UIImage(named: pelicula.tituloPelicula)
         fila.lblTituloPelicula.text = pelicula.tituloPelicula
-        fila.lblFechaEstrenoPelicula.text = ""
-        //fila.imgPelicula.image = pelicula.imagenPelicula ?? UIImage(named: "placeholder")
-        
+        fila.lblFechaEstrenoPelicula.text = pelicula.fechaEstrenoPelicula
+
         return fila
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 161, height: 330)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "detallePelicula", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detallePelicula" {
+            let pantallaDetallePelicula = segue.destination as! DetallePeliculaViewController
+            if let indexPath = cvPelicula.indexPathsForSelectedItems?.first {
+                pantallaDetallePelicula.pelicula = lista[indexPath.row]
+            }
+        }
+    }
+   
 }
--- CineGriff/api/RegistrarPeliculaViewController.swift
+++ CineGriff/api/RegistrarPeliculaViewController.swift
@@ -1,22 +1,110 @@
 import UIKit
-
-class RegistrarPeliculaViewController: UIViewController {
-
+import Alamofire
+class RegistrarPeliculaViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
+    
+    @IBOutlet weak var txtTitulo: UITextField!
+    
+    @IBOutlet weak var txtDescripcion: UITextField!
+    
+    @IBOutlet weak var txtDuracion: UITextField!
+    
+    @IBOutlet weak var txtDirector: UITextField!
+    
+    @IBOutlet weak var txtClasificacion: UITextField!
+    
+    @IBOutlet weak var dpFechaEstreno: UIDatePicker!
+    
+    @IBOutlet weak var pvGenero: UIPickerView!
+    
+    var generos: [Genero] = []
+    var generoSeleccionado: Genero?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pvGenero.delegate=self
+        pvGenero.dataSource=self
+        
+        cargarGeneros()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return generos.count
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return generos[row].nombreGenero
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        generoSeleccionado = generos[row]
+    }
+    
+    func cargarGeneros() {
+            AF.request("https://cinegriffapi-production.up.railway.app/api/genero/listar").responseDecodable(of: [Genero].self) { response in
+                guard let generos = response.value else {
+                    print("Error al obtener los géneros")
+                    return
+                }
+                self.generos = generos
+                self.pvGenero.reloadAllComponents()
+            }
+        }
+    
+    func registrarPelicula(bean:Pelicula){
+        AF.request("https://cinegriffapi-production.up.railway.app/api/pelicula/register",method: .post, parameters: bean, encoder: JSONParameterEncoder.default).response(completionHandler: { data in
+            switch data.result{
+                case .success(let info):
+                do{
+                    let obj =
+                    try JSONDecoder().decode(Pelicula.self, from: info!)
+                    self.ventana("Pelicula guardada con Codigo: \(obj.codigoPelicula)")
+                }catch{
+                    print("Error en el JSON")
+                }
+                case .failure(let error as NSError):
+                print("Error al registrar la película: \(error)")
+            }
+        })
+    }
+    
+    func formatDateToString(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        return dateFormatter.string(from: date)
+    }
 
+
+    @IBAction func btnRegistrarPelicula(_ sender: UIButton) {
+        let cod = 0
+        let tit = String(txtTitulo.text ?? "0")
+        let des = String(txtDescripcion.text ?? "0")
+        let dur = String(txtDuracion.text ?? "0")
+        let dir = String(txtDirector.text ?? "0")
+        let cla = Int(txtClasificacion.text ?? "0") ?? 0
+        let fec = formatDateToString(dpFechaEstreno.date)
+        guard let gen = generoSeleccionado else { print("Por favor, complete todos los campos."); return }
+        let obj = Pelicula(codigoPelicula: cod, tituloPelicula: tit, descripcionPelicula: des, duracionPelicula: dur, directorPelicula: dir, genero: gen, fechaEstrenoPelicula: fec, clasificacionEdad: cla)
+        registrarPelicula(bean: obj)
+    }
+    
+    @IBAction func btnVolver(_ sender: UIButton) {
+        performSegue(withIdentifier: "volverListarPelicula1", sender: nil)
+    }
+    
+    
+    func ventana(_ msg:String){
+        let pantalla=UIAlertController(title: "Sistema", message: msg, preferredStyle: .alert)
+        pantalla.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: {
+            action in
+            self.performSegue(withIdentifier: "volverListarPelicula1", sender: nil)
+        }))
+        present(pantalla, animated: true)
+    }
+    
 }
