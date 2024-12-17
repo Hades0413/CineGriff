2b0ce8c1255ccf0d31007f956af74390c14f5313
Hades0413 <migvel.gomero.1901@hotmail.com>
2024-12-17 14:50:48 -0500

    V2.4.1

--- CineGriff/coredata/GoatViewController.swift
+++ CineGriff/coredata/GoatViewController.swift
@@ -21,25 +21,49 @@ class GoatViewController: UIViewController,UITableViewDataSource,UITableViewDele
         tvGoat.delegate=self
     }
     
-    func datos(){
-        lista.append(Goat(codigoEstudiante: 1, nombreEstudiante: "Eduardo Miguel", apellidoEstudiante: "Jaime Gomero", correoEstudiante: "i202214869@cibertec.edu.pe"))
-    }
+    func datos() {
+    lista.append(Goat(codigoEstudiante: 1, 
+                       nombreEstudiante: "Jorge Fabrizio", 
+                       apellidoEstudiante: "Olano Farfan", 
+                       correoEstudiante: "i202214869@cibertec.edu.pe"))
     
+    lista.append(Goat(codigoEstudiante: 2, 
+                       nombreEstudiante: "Marcelo Adrian", 
+                       apellidoEstudiante: "Liendo Soto", 
+                       correoEstudiante: "i202214870@cibertec.edu.pe"))
+    
+    lista.append(Goat(codigoEstudiante: 3, 
+                       nombreEstudiante: "Claudia Yadira", 
+                       apellidoEstudiante: "Sifuentes Zevallos", 
+                       correoEstudiante: "i202214871@cibertec.edu.pe"))
+    
+    lista.append(Goat(codigoEstudiante: 4, 
+                       nombreEstudiante: "Adriana Sofia", 
+                       apellidoEstudiante: "Casas Durand", 
+                       correoEstudiante: "i202214872@cibertec.edu.pe"))
+    
+    lista.append(Goat(codigoEstudiante: 5, 
+                       nombreEstudiante: "Eduardo Miguel", 
+                       apellidoEstudiante: "Jaime Gomero", 
+                       correoEstudiante: "i202214869@cibertec.edu.pe"))
+}
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lista.count //cantidad de elementos que hay dentro del arreglo de lista
+        return lista.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //crear objeto de la clase ItemTableViewCell
-        let fila = tvGoat.dequeueReusableCell(withIdentifier: "row") as! ItemGoatTableViewCell
-        
-        fila.lblCodigo.text = String(lista[indexPath.row].codigoEstudiante)
-        fila.lblNombre.text = lista[indexPath.row].nombreEstudiante
-        fila.lblApellido.text = lista[indexPath.row].apellidoEstudiante
-        fila.lblCorreo.text = lista[indexPath.row].correoEstudiante
-        
-        return fila
-    }
+
+    let fila = tvGoat.dequeueReusableCell(withIdentifier: "row") as! ItemGoatTableViewCell
+    
+    fila.lblCodigo.text = "Código: \(lista[indexPath.row].codigoEstudiante)"
+    fila.lblNombre.text = "Nombre: \(lista[indexPath.row].nombreEstudiante)"
+    fila.lblApellido.text = "Apellido: \(lista[indexPath.row].apellidoEstudiante)"
+    fila.lblCorreo.text = "Correo: \(lista[indexPath.row].correoEstudiante)"
+    
+    return fila
+}
+
 
 }
